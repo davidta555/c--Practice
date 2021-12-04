a0ee947ec7921dfda77f4198b8dd53c193a4f925
David Ta <david.ta555@gmail.com>
2021-12-04 14:01:54 -0500

    Read Board from Another File

--- createBoard.cpp
+++ createBoard.cpp
@@ -1,7 +1,22 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 #include <vector>
+using std::ifstream;
 using std::cout;
+using std::string;
 using std::vector;
+
+void ReadBoardFile(string path) {
+	ifstream my_file (path);
+  	if (my_file) {
+        string line;
+        while (getline(my_file, line)) {
+            cout << line << "\n";
+      }
+   }
+}
+
 
 void PrintBoard(const vector<vector<int>> b) {
   	for (auto v : b) {
@@ -13,13 +28,14 @@ void PrintBoard(const vector<vector<int>> b) {
 }
 
 int main() {
-    vector<vector<int>> board{
-        {0, 1, 0, 0, 0, 0},
-        {0, 1, 0, 0, 0, 0},
-        {0, 1, 0, 0, 0, 0},
-        {0, 1, 0, 0, 0, 0},
-        {0, 0, 0, 0, 1, 0}
-    };
+    // vector<vector<int>> board {
+    //     {0, 1, 0, 0, 0, 0},
+    //     {0, 1, 0, 0, 0, 0},
+    //     {0, 1, 0, 0, 0, 0},
+    //     {0, 1, 0, 0, 0, 0},
+    //     {0, 0, 0, 0, 1, 0}
+    // };
     
-    PrintBoard(board);
+    ReadBoardFile("board");
+    // PrintBoard(board);
 }
