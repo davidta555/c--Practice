f1d161059395b16afc4f3c6b593669c57e2dc6e4
David Ta <david.ta555@gmail.com>
2021-12-14 16:30:39 -0500

    Adding Nodes to Open Vector

--- createBoard.cpp
+++ createBoard.cpp
@@ -9,7 +9,7 @@ using std::istringstream;
 using std::string;
 using std::vector;
 
-enum class State {kEmpty, kObstacle};
+enum class State {kEmpty, kObstacle, kClosed};
 
 vector<State> ParseLine(string line) {
   istringstream my_stream(line);
@@ -44,6 +44,20 @@ vector<vector<State>> ReadBoardFile(string path) {
    return board;
 }
 
+int Heuristic(int x1, int x2, int y1, int y2) {
+	return abs(x2 - x1) + abs(y2 - y1);
+}
+
+void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &openlist, vector<vector<State>> &grid) {
+	openlist.push_back(vector<int>{x, y, g, h});
+	grid[x][y] = State::kClosed;
+}
+
+vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2]) {
+	cout << "No path found!" << "\n";
+  	return std::vector<vector<State>> {};
+}
+
 string CellString(State cell) {
   switch(cell) {
     case State::kObstacle: return "⛰️   ";
@@ -61,14 +75,18 @@ void PrintBoard(const vector<vector<State>> board) {
 }
 
 int main() {
-    // vector<vector<int>> board {
-    //     {0, 1, 0, 0, 0, 0},
-    //     {0, 1, 0, 0, 0, 0},
-    //     {0, 1, 0, 0, 0, 0},
-    //     {0, 1, 0, 0, 0, 0},
-    //     {0, 0, 0, 0, 1, 0}
-    // };
+  // vector<vector<int>> board {
+  //     {0, 1, 0, 0, 0, 0},
+  //     {0, 1, 0, 0, 0, 0},
+  //     {0, 1, 0, 0, 0, 0},
+  //     {0, 1, 0, 0, 0, 0},
+  //     {0, 0, 0, 0, 1, 0}
+  // };
     
-    auto board = ReadBoardFile("boardFile");
-    PrintBoard(board);
+  int init[2]{0, 0};
+  int goal[2]{4, 5};
+  auto board = ReadBoardFile("boardFile");
+
+  auto solution = Search(board, init, goal);
+  PrintBoard(solution);
 }
